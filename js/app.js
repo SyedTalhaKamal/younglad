c95209da06a4407fbb9e18aada41dc824217c705
SyedTalhaKamal <syedtalhakamal22@gmail.com>
2020-10-12 19:45:40 +0500

    Practice Work in Salsoft Tecgnologies,not for use

---
+++ js/app.js
@@ -0,0 +1,24 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:2
+        }
+    }
+})
